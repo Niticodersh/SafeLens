26edc9ae82aaa39fbb15395b1107153c0d7e49e2
Nitish Bhardwaj <98891876+Niticodersh@users.noreply.github.com>
2024-10-15 02:37:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ import { getDatabase, ref as dbRef, set, push, get } from 'https://www.gstatic.c
 
 // Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB9OxGdh3dVrFDFHmyAWtt1WAASrPsf3CA",
-  authDomain: "safelens.firebaseapp.com",
-  databaseURL: "https://safelens-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "safelens",
-  storageBucket: "safelens.appspot.com",
-  messagingSenderId: "158160811936",
-  appId: "1:158160811936:web:da72ab251d037e9f6356c5",
-  measurementId: "G-C6K9TSX302"
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
